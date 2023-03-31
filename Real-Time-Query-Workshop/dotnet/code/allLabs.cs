a2c4a77f8f3d97c1388068e4d7ee98a1422fb07b
joeywhelan <joey.whelan@redis.com>
2023-03-31 11:23:05 -0600

    Fix dotnet allLabs

--- Real-Time-Query-Workshop/dotnet/code/allLabs.cs
+++ Real-Time-Query-Workshop/dotnet/code/allLabs.cs
@@ -13,19 +13,19 @@ public class AllLabs
         Lab1 lab1 = new Lab1();
         IDatabase db = lab1.Run();
 
-        //Lab2 lab2 = new Lab2();
-        //lab2.Run(db);
+        Lab2 lab2 = new Lab2();
+        lab2.Run(db);
 
-        //Lab3 lab3 = new Lab3();
-        //lab3.Run(db);
+        Lab3 lab3 = new Lab3();
+        lab3.Run(db);
 
-        //Lab4 lab4 = new Lab4();
-        //lab4.Run(db);
+        Lab4 lab4 = new Lab4();
+        lab4.Run(db);
 
         Lab5 lab5 = new Lab5();
         lab5.Run(db);
 
-        //Lab6 lab6 = new Lab6();
-        //lab6.Run(db);
+        Lab6 lab6 = new Lab6();
+        lab6.Run(db);
     }
 }
